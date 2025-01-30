c7e67b5222796ef66dd6b68e3643ed9776b31b0e
camdao <luongcamdao@gmail.com>
2025-01-30 14:44:05 +0700

    feat: config mysqli

---
+++ resources/settings.php
@@ -0,0 +1,8 @@
+<?php
+$configDatabase = [
+    'db_host' => 'localhost',
+    'db_user' => 'root',
+    'db_pass' => '',
+    'db_database' => 'my_database'
+];
+?>
---
+++ src/global/config/mysqli.php
@@ -0,0 +1,19 @@
+<?php
+    
+    function connectDatabase(){
+        include __DIR__ . '/resources/settings.php';
+        $host = $configDatabase['db_host'];
+        $user = $configDatabase['db_user'];
+        $pass = $configDatabase['db_pass'];
+        $database = $configDatabase['db_database'];
+        
+        $conn = new mysqli($host, $user, $pass, $database);
+
+        if($conn){
+            return $conn;
+        }else{
+            echo"Could not connect database";
+        }
+    }
+
+?>
